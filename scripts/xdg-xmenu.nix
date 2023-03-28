728e764de90ad6291a22e1226ffc7e459a66f3bc
jzbor <jzbor@posteo.net>
2023-03-29 01:29:20 +0200

    Adding xdg-xmenu script

---
+++ scripts/xdg-xmenu.nix
@@ -0,0 +1,18 @@
+{ lib, stdenv, pkgs, ... }:
+
+let
+  script-name = "xdg-xmenu";
+  script-src = builtins.readFile ./xdg-xmenu.py;
+  script = (pkgs.writeShellScriptBin script-name script-src).overrideAttrs(old: {
+    buildCommand = "${old.buildCommand}\n patchShebangs $out";
+  });
+  script-inputs = with pkgs; [
+    imagemagick
+    python3
+  ];
+in pkgs.symlinkJoin {
+  name = script-name;
+  paths = [ script ] ++ script-inputs;
+  buildInputs = [ pkgs.makeWrapper ];
+  postBuild = "wrapProgram $out/bin/${script-name} --prefix PATH : $out/bin";
+}
