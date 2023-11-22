d16f4dd415459fea16ff9da0acb29e535a2967d4
jzbor <jzbor@posteo.net>
2023-11-22 09:21:55 +0100

    Fixing missing touchegg config dependencies

--- programs/touchegg/default.nix
+++ programs/touchegg/default.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ touchegg ];
+  home.packages = with pkgs; [ touchegg xdotool ];
 
   xdg.configFile."touchegg/touchegg.conf".source = ./touchegg.xml;
 }
